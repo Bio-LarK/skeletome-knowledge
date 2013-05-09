0d4110e6bba6f4f79999cf0fadd4d3d8f975179d
Craig <cmcnamara87@gmail.com>
2013-05-09 14:42:44 +1000

    Add: Skeletome about module Add: Skeletome dashboard structure

---
+++ sites/all/modules/custom/skeletome_about/templates/about_page.tpl.php
@@ -0,0 +1,12 @@
+<div class="row">
+    <div class="span12">
+        <div class="page-heading">
+            <h1>About</h1>
+        </div>
+        <section>
+            <div class="section-segment">
+                A community-driven knowledge curation platform for skeletal dysplasias.
+            </div>
+        </section>
+    </div>
+</div>
---
+++ sites/all/modules/custom/skeletome_dashboard/templates/dashboard_page.tpl.php
@@ -0,0 +1,51 @@
+<div>
+    <div class="row">
+        <div class="span12">
+            <div class="page-heading">
+                <h1>Dashboard</h1>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="span4">
+            <section>
+                <div class="section-segment section-segment-heading">
+                    <h2>Recent Activity</h2>
+                </div>
+            </section>
+        </div>
+
+        <div class="span4">
+            <section>
+                <div class="section-segment section-segment-heading">
+                    <h2>Monitoring</h2>
+                </div>
+            </section>
+        </div>
+
+        <div class="span4">
+            <section>
+                <div class="section-segment section-segment-heading">
+                    <h2>Recent Searches</h2>
+                </div>
+            </section>
+        </div>
+
+        <div class="span4">
+            <section>
+                <div class="section-segment section-segment-heading">
+                    <h2>Messages</h2>
+                </div>
+            </section>
+        </div>
+
+        <div class="span4">
+            <section>
+                <div class="section-segment section-segment-heading">
+                    <h2>Top 5 Searches</h2>
+                </div>
+            </section>
+        </div>
+
+    </div>
+</div>
